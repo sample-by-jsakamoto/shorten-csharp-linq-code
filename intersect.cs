8957c0b8df57a017f80ce016b9cb03e73269c6ad
jsakamoto <junichi.jp@gmail.com>
2014-08-14 23:25:44 +0900

    We can write method chain style code. then, we succeeded in reducing number of variable.

--- intersect.cs
+++ intersect.cs
@@ -4,8 +4,8 @@ var listA = new [] {"A","B","C","D","E"};
 // List To Compare with String 1,A,4,E,5
 var listB = new [] {"1","A","4","E","5"};
 
-// Retrieve the List of String which appear on both List
-var matchList = listA.Intersect(listB).ToList();
-
-// Output the list of matched string
-matchList.ForEach(Console.WriteLine);
+// Retrieve the List of String which appear on both List,
+// and output it.
+listA.Intersect(listB)
+	.ToList()
+	.ForEach(Console.WriteLine);

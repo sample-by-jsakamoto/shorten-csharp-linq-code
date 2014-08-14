29af36b733b576187b6de466cbd651636f952695
jsakamoto <junichi.jp@gmail.com>
2014-08-14 23:22:17 +0900

    We can also simple array instead of List<T>. LINQ can apply simple array, too.

--- intersect.cs
+++ intersect.cs
@@ -1,8 +1,8 @@
 // Source List with String A,B,C,D,E
-var listA = new List<string> {"A","B","C","D","E"};
+var listA = new [] {"A","B","C","D","E"};
 
 // List To Compare with String 1,A,4,E,5
-var listB = new List<string> {"1","A","4","E","5"};
+var listB = new [] {"1","A","4","E","5"};
 
 // Retrieve the List of String which appear on both List
 var matchList = listA.Intersect(listB).ToList();

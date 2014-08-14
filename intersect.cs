23ca76c3108ffa30e34225433d59abaf4730387b
jsakamoto <junichi.jp@gmail.com>
2014-08-14 23:17:23 +0900

    We can use "collection initializer".

--- intersect.cs
+++ intersect.cs
@@ -1,18 +1,8 @@
 // Source List with String A,B,C,D,E
-var listA = new List<string>();
-listA.Add("A");
-listA.Add("B");
-listA.Add("C");
-listA.Add("D");
-listA.Add("E");
+var listA = new List<string> {"A","B","C","D","E"};
 
 // List To Compare with String 1,A,4,E,5
-var listB = new List<string>();
-listB.Add("1");
-listB.Add("A");
-listB.Add("4");
-listB.Add("E");
-listB.Add("5");
+var listB = new List<string> {"1","A","4","E","5"};
 
 // Retrieve the List of String which appear on both List
 var matchList = listA.Intersect(listB).ToList();

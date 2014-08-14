6da299276deb70fc788cf4595a830923f7635045
jsakamoto <junichi.jp@gmail.com>
2014-08-14 23:20:55 +0900

    We can use "ForEach" extension method instead of "for" loop. This effect deletion variable "i".

--- intersect.cs
+++ intersect.cs
@@ -7,8 +7,5 @@ var listB = new List<string> {"1","A","4","E","5"};
 // Retrieve the List of String which appear on both List
 var matchList = listA.Intersect(listB).ToList();
 
-for (int i = 0; i < matchList.Count; i++)
-{
-	// Output the list of matched string
-	Console.WriteLine(matchList[i]);
-}
+// Output the list of matched string
+matchList.ForEach(Console.WriteLine);
